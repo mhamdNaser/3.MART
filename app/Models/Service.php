9f6855b919baf3d1480fc83ce377d9fe35bd6fd7
unknown <mohammadalmobayed4@gmail.com>
2023-01-24 16:06:53 +0200

    Databaiss

---
+++ app/Models/Service.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Reservation;
+use App\Models\Category;
+
+class Service extends Model
+{
+    use HasFactory;
+    public function cat (){
+        return $this->belongsTo(Category::class);
+    }
+    public function Reservation1 (){
+        return $this->hasMany(Reservation::class);
+    }
+    protected $fillable = [
+        'Service_Name',
+        'Category_id',
+        'Service_Description',
+        'Service_Image',
+        'Service_Price',
+        'Service_Duration',
+       
+    ];
+}
---
+++ database/migrations/2023_01_24_120011_create_reservations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("Service_Id")->constrained("services")->cascadeOnDelete();
+            $table->foreignId("User_Id")->constrained("users")->cascadeOnDelete();
+            $table->string("City");
+            $table->string("Street_Name");
+            $table->string("Building_Number");
+            $table->dateTime("Start_Time");
+            $table->dateTime("End_Time")->nullable();
+            $table->double("Total_Price")->nullable();
+            $table->string("Status")->default("Pending");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
