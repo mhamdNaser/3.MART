65a502a77c33d5e1f04425bae3db7cc8fa40927c
unknown <ruba.salahuddin@gmail.com>
2023-01-25 21:23:22 +0300

    Udate User

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -18,6 +18,7 @@
 
         <link href="assets/css/app.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/6487c144b2.js" crossorigin="anonymous"></script>
     </head>
 </head>
 <body>
