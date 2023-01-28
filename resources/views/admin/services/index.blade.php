5f95bd725c803f592a1992150d07b645f478b4fe
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2023-01-28 23:58:04 +0300

    dashboard style

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -1,61 +1,79 @@
 @extends('layout.navbar')
-@section('title')
-Services
-@endsection
-@section('service_active')
-active
-@endsection
-@section('container')
+@section('title', 'Services')
+@section('service_active', 'active')
 
-    <div class="container mt-2">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <h2>Service</h2>
-                </div>
-                <div class="pull-right mb-2">
-                    <a class="btn btn-success" href="{{ route('Service.create') }}"> Add New Service</a>
-                </div>
+@section('button')
+    <a href="{{route('Service.create')}}"><button type="button" class="btn btn-dark" >Add New Service</button></a>
+@endsection
+
+@section('container')
+<div class="container-fluid py-4">
+    <div class="row">
+        <div class="col-12">
+            <div class="card my-4">
+                <h3 class="p-3 text-capitalize text-dark text-center fw-bold">Service Table</h3>
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success">
+                        <p>{{ $message }}</p>
+                    </div>
+                @endif
+                <table class="table align-items-center mb-0 shadow-lg">
+                    <thead>
+                        <tr>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                service_name
+                            </th>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Category Id
+                            </th>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Service Description
+                            </th>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Service_Image
+                            </th>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Service_Price
+                            </th>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Service_Duration
+                            </th>
+                            <th scope="row" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($services as $service)
+                        <tr>
+                            <td class="align-middle text-center">{{ $service->Service_Name }}</td>
+                            <td class="align-middle text-center">{{ $service->Category_id }}</td>
+                            <td class="align-middle text-center">{{ $service->Service_Description }}</td>
+                            <td class="align-middle text-center">
+                                <img src="{{URL::asset("storage/serviceimage/$service->Service_Image")}}" class="avatar avatar-sm me-3 border-radius-lg">
+                            </td>
+                            <td class="align-middle text-center">{{ $service->Service_Price }}</td>
+                            <td class="align-middle text-center">{{ $service->Service_Duration }}</td>
+                            <td class="align-middle text-center">
+                                <div class="d-flex">
+                                    <div class="p-1  border border-0">
+                                    <form action="{{ route('Service.destroy',$service->id) }}" method="Post">
+
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </div>
+                                <div class="p-1  border border-0">
+                                    <a class="btn btn-primary" href="{{route('Service.edit',$service->id) }}">Edit</a>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>service_name</th>
-                    <th>categoryid</th>
-                    <th>Service_Description</th>
-                    <th>Service_Image</th> 
-                    <th>Service_Price</th> 
-                    <th>Service_Duration</th> 
-                    <th width="280px">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($services as $service)
-                    <tr>
-                      <td>{{ $service->Service_Name }}</td>
-                      {{-- <td>{{ $service->Category_id }}</td> --}}
-                      <td>{{ $service->Service_Description }}</td>
-                        <td><img src="{{URL::asset("storage/serviceimage/$service->Service_Image")}}" alt="" srcset="" style="width:100px;"></td>
-                        <td>{{ $service->Service_Price }}</td>
-                        <td>{{ $service->Service_Duration }}</td>
-                        <td>
-                            <form action="{{ route('Service.destroy',$service->id) }}" method="Post">
-                              <a class="btn btn-primary" href="{{route('Service.edit',$service->id) }}">Edit</a>
-                              @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-            </tbody>
-        </table>
     </div>
+</div>
 @endsection
-
